3adb9e661e0ff075f95ceec586647a1f76d5b96a
Tomasz Solik <tomasz.solik@gmail.com>
2018-09-28 12:54:35 +0200

    Added starting message

--- server/server.js
+++ server/server.js
@@ -15,14 +15,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    io.emit('newMessage', {
-        from: 'Test',
-        text: 'test message',
-        createdAt: 123
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
     })
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: 'Test',
+            text: 'test message',
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
