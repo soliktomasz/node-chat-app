64fbb901e2b845c53305345bcefef51260b77306
Solik Tomasz <tomasz.solik@etteplan.com>
2018-10-03 13:36:33 +0200

    Added geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('Should generate correct message object', () => {
@@ -9,6 +9,18 @@ describe('generateMessage', () => {
         var message = generateMessage(from, text);
 
         expect(typeof message.createdAt).toBe('number');
-        expect(message).toMatchObject({from, text });
+        expect(message).toMatchObject({ from, text });
+    })
+});
+
+describe('generateLocationMessage', () => {
+    it('Should generate correct location message object', () => {
+        var from = "Test";
+        var latitude = "52.4112655";
+        var longitude = "16.9405408";
+        var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+        expect(typeof locationMessage.createdAt).toBe('number');
+        expect(locationMessage).toMatchObject({ from, url: `https://google.com/maps?q=${latitude},${longitude}` });
     })
 });
