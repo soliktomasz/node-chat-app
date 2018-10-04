993ed2f52a8ff900b15a5c114eecd587bde00cb7
Tomasz Solik <tomasz.solik@gmail.com>
2018-10-04 11:46:55 +0200

    Added Users functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,7 +17,16 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function () {
-	console.log('Connected to server');
+	var params = jQuery.deparam(window.location.search);
+
+	socket.emit('join', params, function (err) {
+		if (err) {
+			alert(err);
+			window.location.href = '/';
+		} else {
+			console.log('No error');
+		}
+	});
 });
 
 socket.on('newMessage', function (message) {
@@ -35,6 +44,15 @@ socket.on('newMessage', function (message) {
 
 socket.on('disconnect', function () {
 	console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+	var ol = jQuery('<ol></ol>');
+
+	users.forEach(function (user) {
+		ol.append(jQuery('<li></li>').text(user));
+	});
+	jQuery('#users').html(ol);
 });
 
 socket.on('newLocationMessage', function (message) {
